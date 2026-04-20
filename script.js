5b10850418351ee95d99ded31f1a05324274556e
Linus Lindström <linus@datasnillet.com>
2026-04-20 09:33:52 +0200

    First method done

--- script.js
+++ script.js
@@ -6,16 +6,32 @@ let article1 = {
     isTea:true,
     name:"Sleepy Time"
 }
+let article2 = {
+    price:119,
+    isTea:false,
+    name:"Cup Galore"
+}
+
 articles.push(article1);
+articles.push(article2);
+articles.push(article1);
+articles.push(article2);
 
 
-function LoadArticles(){
+function LoadArticles(input){
+    main.innerHTML="";
     articles.forEach(article => {
-    let div = document.createElement("div");
-    div.class="ruta";
-    let Price = document.createElement("p");
-    Price.textContent=article.price;
-    div.appendChild(Price);
-    main.appendChild(div);
+        if ((article.isTea&&input!=3)||(!article.isTea&&input!=1)||(input==2))
+        {
+            let div = document.createElement("div");
+            div.className="ruta";
+            let Title = document.createElement("h2");
+            Title.textContent=article.name;
+            let Price = document.createElement("h3");
+            Price.textContent=article.price+":-";
+            div.appendChild(Title)
+            div.appendChild(Price);
+            main.appendChild(div);
+        }
     });
 }

42eaf2234d7fb9e57441f7bcded33420ed735308
Linus Lindström <linus@datasnillet.com>
2026-04-21 14:10:15 +0200

    RWD done, started on 2nd page

--- script.js
+++ script.js
@@ -12,24 +12,43 @@ let article2 = {
     name:"Cup Galore"
 }
 
-articles.push(article1);
-articles.push(article2);
-articles.push(article1);
-articles.push(article2);
-
+AddArticles(4)
+function AddArticles(input){
+    for (let x = 0; x<input; x++)
+    {
+        console.log("loop")
+        articles.push(article1);
+        articles.push(article2);
+    }
+}
 
 function LoadArticles(input){
     main.innerHTML="";
     articles.forEach(article => {
-        if ((article.isTea&&input!=3)||(!article.isTea&&input!=1)||(input==2))
+        if ((article.isTea&&input!=3)||(!article.isTea&&input!=1))
         {
             let div = document.createElement("div");
             div.className="ruta";
+
             let Title = document.createElement("h2");
             Title.textContent=article.name;
+
+            let Image = document.createElement("img")
+            if (!article.isTea)
+            {
+                Image.src = "./img/TeKopp.png"
+            }
+            else
+            {
+                Image.src = "./img/SleepyTime.png"
+            }
+            Image.alt = article.name
+
             let Price = document.createElement("h3");
             Price.textContent=article.price+":-";
+            
             div.appendChild(Title)
+            div.appendChild(Image);
             div.appendChild(Price);
             main.appendChild(div);
         }

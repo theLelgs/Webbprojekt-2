ef7cd50f45fb03926c60ad931b68f83f8494028f
Linus Lindström <linus@datasnillet.com>
2026-04-22 09:16:40 +0200

    Randomizing the products, moved the local storage call for store page

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let articles = [];
 
 let teaNamesPartOne = ["Sleepy", "Energizing", "Calming", "Green"]
 let teaNamesPartTwo = ["Time", "Tea", "Deluxe", "Ultra"]
-let cupNamesPartOne = ["Cup", "Porcelain", "Luxury", "The Perfect"]
+let cupNamesPartOne = ["Cup O'", "Porcelain", "Luxury", "The Perfect"]
 let cupNamesPartTwo = ["Galore", "Deluxe", "Ultra", "Tea"]
 
 let ShoppingList = [];
@@ -13,33 +13,31 @@ if (localStorage.getItem("ShoppingList")!=null)
     ShoppingList = JSON.parse(localStorage.getItem("ShoppingList"));
 }
 
-AddArticles(4)
+AddArticles(15)
 function AddArticles(input){
     for (let x = 0; x<input; x++)
     {
-        let teaArticle = {
-            price:Math.floor(Math.random()*40)+70,
-            isTea:true,
-            name:teaNamesPartOne[Math.floor(Math.random(0,1)*4)] + " " + teaNamesPartTwo[Math.floor(Math.random(0,1)*4)]
+        let coinflip = Math.floor(Math.random()*2)
+        let article = {
+            price:Math.floor(Math.random()*5)*10+79
         }
-        let cupArticle = {
-            price:Math.floor(Math.random()*50+80),
-            isTea:false,
-            name:cupNamesPartOne[[Math.floor(Math.random(0,1)*4)]] + " " + cupNamesPartTwo[[Math.floor(Math.random(0,1)*4)]]
+        if (coinflip<1)
+        {
+            article.isTea=true;
+            article.name=teaNamesPartOne[Math.floor(Math.random(0,1)*4)] + " " + teaNamesPartTwo[Math.floor(Math.random(0,1)*4)]
         }
-        articles.push(teaArticle);
-        articles.push(cupArticle);
+        else
+        {
+            article.isTea=false;
+            article.name=cupNamesPartOne[[Math.floor(Math.random(0,1)*4)]] + " " + cupNamesPartTwo[[Math.floor(Math.random(0,1)*4)]]
+        }
+        articles.push(article);
     }
 }
 
 function AddToShoppingList(article){
     ShoppingList.push(article)
-}
-
-
-function GoToShoppingList(){
-    let list = JSON.stringify(ShoppingList)
-    localStorage.setItem("ShoppingList", list)
+    localStorage.setItem("ShoppingList", JSON.stringify(ShoppingList))
 }
 
 function LoadArticles(input){
